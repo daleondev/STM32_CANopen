9a19e51776cf0997fc78f50ea5481dc6f3758d35
daleondev <david@rodenheber.de>
2026-03-10 19:45:23 +0100

    clean cmake

--- Platform.cmake
+++ Platform.cmake
@@ -1,76 +1,111 @@
+include_guard(GLOBAL)
+
+set(PLATFORM_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}")
+set(PLATFORM_EXTERN_DIR "${PLATFORM_ROOT_DIR}/extern")
+set(CUBEMX_DIR "${PLATFORM_EXTERN_DIR}/CubeMX")
+set(HAL_DIR "${PLATFORM_EXTERN_DIR}/stm32h7xx-hal-driver")
+set(CMSIS_CORE_DIR "${PLATFORM_EXTERN_DIR}/cmsis-core")
+set(CMSIS_DEVICE_DIR "${PLATFORM_EXTERN_DIR}/cmsis-device-h7")
+set(NUCLEO_BSP_DIR "${PLATFORM_EXTERN_DIR}/stm32h7xx-nucleo-bsp")
+set(THREADX_DIR "${PLATFORM_EXTERN_DIR}/threadx")
+
+function(platform_prepend out_var base_dir)
+    set(result)
+    foreach(file IN LISTS ARGN)
+        list(APPEND result "${base_dir}/${file}")
+    endforeach()
+    set(${out_var} "${result}" PARENT_SCOPE)
+endfunction()
+
 set(THREADX_ARCH cortex_m7)
 set(THREADX_TOOLCHAIN gnu)
 set(TX_ENABLE_STACK_CHECKING ON)
-set(TX_USER_FILE ${CMAKE_SOURCE_DIR}/extern/CubeMX/Inc/tx_user.h)
-add_subdirectory(${CMAKE_SOURCE_DIR}/extern/threadx)
+set(TX_USER_FILE "${CUBEMX_DIR}/Inc/tx_user.h")
+
+add_subdirectory("${THREADX_DIR}")
+
+platform_prepend(PLATFORM_HAL_SOURCES "${HAL_DIR}/Src"
+    stm32h7xx_hal_tim.c
+    stm32h7xx_hal_tim_ex.c
+    stm32h7xx_hal_cortex.c
+    stm32h7xx_hal_eth.c
+    stm32h7xx_hal_eth_ex.c
+    stm32h7xx_hal_rcc.c
+    stm32h7xx_hal_rcc_ex.c
+    stm32h7xx_hal_flash.c
+    stm32h7xx_hal_flash_ex.c
+    stm32h7xx_hal_gpio.c
+    stm32h7xx_hal_hsem.c
+    stm32h7xx_hal_dma.c
+    stm32h7xx_hal_dma_ex.c
+    stm32h7xx_hal_mdma.c
+    stm32h7xx_hal_pwr.c
+    stm32h7xx_hal_pwr_ex.c
+    stm32h7xx_hal.c
+    stm32h7xx_hal_i2c.c
+    stm32h7xx_hal_i2c_ex.c
+    stm32h7xx_hal_exti.c
+    stm32h7xx_hal_rng.c
+    stm32h7xx_hal_rng_ex.c
+    stm32h7xx_hal_rtc.c
+    stm32h7xx_hal_rtc_ex.c
+    stm32h7xx_hal_usart.c
+    stm32h7xx_hal_usart_ex.c
+    stm32h7xx_hal_uart_ex.c
+    stm32h7xx_hal_uart.c
+)
+
+platform_prepend(PLATFORM_CUBEMX_SOURCES "${CUBEMX_DIR}/Src"
+    main.c
+    gpio.c
+    rng.c
+    rtc.c
+    tim.c
+    stm32h7xx_it.c
+    stm32h7xx_hal_msp.c
+    stm32h7xx_hal_timebase_tim.c
+    sysmem.c
+    syscalls.c
+    system_stm32h7xx.c
+    tx_initialize_low_level.S
+)
+
+set(PLATFORM_STARTUP_SOURCES
+    "${CUBEMX_DIR}/startup_stm32h753xx.s"
+)
+
+set(PLATFORM_BSP_SOURCES
+    "${NUCLEO_BSP_DIR}/stm32h7xx_nucleo.c"
+)
 
 add_library(Platform STATIC)
 
 target_sources(Platform
     PRIVATE
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_tim.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_tim_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_cortex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_eth.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_eth_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_rcc.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_rcc_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_flash.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_flash_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_gpio.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_hsem.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_dma.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_dma_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_mdma.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_pwr.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_pwr_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_i2c.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_i2c_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_exti.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_rng.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_rng_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_rtc.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_rtc_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_usart.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_usart_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_uart_ex.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Src/stm32h7xx_hal_uart.c
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-nucleo-bsp/stm32h7xx_nucleo.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/main.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/gpio.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/rng.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/rtc.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/tim.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/stm32h7xx_it.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/stm32h7xx_hal_msp.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/stm32h7xx_hal_timebase_tim.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/sysmem.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/syscalls.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/startup_stm32h753xx.s
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/system_stm32h7xx.c
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Src/tx_initialize_low_level.S
+        ${PLATFORM_HAL_SOURCES}
+        ${PLATFORM_BSP_SOURCES}
+        ${PLATFORM_CUBEMX_SOURCES}
+        ${PLATFORM_STARTUP_SOURCES}
 )
 
-target_include_directories(Platform 
+target_include_directories(Platform
     PUBLIC
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Inc
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-hal-driver/Inc/Legacy
-        ${CMAKE_SOURCE_DIR}/extern/stm32h7xx-nucleo-bsp
-        ${CMAKE_SOURCE_DIR}/extern/cmsis-core/Include
-        ${CMAKE_SOURCE_DIR}/extern/cmsis-device-h7/Include
-        ${CMAKE_SOURCE_DIR}/extern/CubeMX/Inc
+        ${HAL_DIR}/Inc
+        ${HAL_DIR}/Inc/Legacy
+        ${NUCLEO_BSP_DIR}
+        ${CMSIS_CORE_DIR}/Include
+        ${CMSIS_DEVICE_DIR}/Include
+        ${CUBEMX_DIR}/Inc
 )
 
-target_compile_definitions(Platform 
+target_compile_definitions(Platform
     PUBLIC
         USE_PWR_LDO_SUPPLY
         USE_NUCLEO_64
         USE_HAL_DRIVER
         STM32H753xx
-        $<$<CONFIG:Debug>:DEBUG>
-
         TX_INCLUDE_USER_DEFINE_FILE
+        $<$<CONFIG:Debug>:DEBUG>
 )
 
 target_link_libraries(Platform
